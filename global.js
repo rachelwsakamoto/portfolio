3ad986b57d2281ad6f33027895361b3d0187b0ba
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-16 19:31:02 -0700

    nav menu

--- global.js
+++ global.js
@@ -3,7 +3,29 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
-currentLink.classList.add('current');
+
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink.classList.add('current');
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  {url:'contact/', title: "Contact"},
+  {url: 'resume/', title: "Resume"}
+  // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+}
+
+// Create link and add it to nav
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
