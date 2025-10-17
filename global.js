33240c213d6edd22deacece7d72cba474fafa62a
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-16 21:28:27 -0700

    Update global.js

--- global.js
+++ global.js
@@ -23,13 +23,11 @@ for (let p of pages) {
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
+  a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+    );
   nav.append(a);
 
 }
-if (a.host === location.host && a.pathname === location.pathname) {
-  a.classList.add('current');
-}
-a.classList.toggle(
-  'current',
-  a.host === location.host && a.pathname === location.pathname,
-);
+
