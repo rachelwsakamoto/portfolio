8f3610f8e9ef5d91b9fc764fc564e7289ae71fa7
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-16 23:25:45 -0700

    Update global.js

--- global.js
+++ global.js
@@ -53,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
     select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
     document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
     });
 });
 
