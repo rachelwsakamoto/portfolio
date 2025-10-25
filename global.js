ee430c8c0be2cf2039543cbf227358e4c2821a79
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-24 23:32:06 -0700

    retrying global

--- global.js
+++ global.js
@@ -1,81 +1,98 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? "/"             
-    : "/portfolio/"; 
-    
-    document.body.insertAdjacentHTML(
-    'afterbegin',
-    `
-        <label class="color-scheme">
-            Theme:
-            <select id="theme-select" name="theme">
-                <option value = "light dark"> Automatic</option>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-            </select>
-        </label>`
-    );
+console.log('IT’S ALIVE!');
 
-    let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'resume/', title: 'Resume' },
-    // add the rest of your pages here
-    ];
-
-    let nav = document.createElement('nav');
-    document.body.append(nav);
-
-    for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
-    
-    // Create link and add it to nav
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname,
-        );
-    nav.prepend(a);
-
-    }
-    const select = document.querySelector('.color-scheme select');
-    if ("colorScheme" in localStorage) {
-        const savedScheme = localStorage.colorScheme;
-        document.documentElement.style.setProperty('color-scheme', savedScheme);
-        select.value = savedScheme;
+function $$(selector, context = document) {
+	return Array.from(context.querySelectorAll(selector));
 }
-    select.addEventListener('input', function (event) {
-    console.log('color scheme changed to', event.target.value);
-    document.documentElement.style.setProperty('color-scheme', event.target.value);
-    localStorage.colorScheme = event.target.value;
-    });
+
+let pages = [
+	{ url: '', title: 'Home' },
+	{ url: 'projects/', title: 'Projects' },
+	{ url: 'resume/', title: 'CV' },
+	{ url: 'contact/', title: 'Contact' },
+	{ url: 'https://github.com/rachelwsakamoto', title: 'GitHub' }
+];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+	? "/"
+	: "/portfolio/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+	let url = p.url;
+	let title = p.title;
+
+	if (!url.startsWith('http')) {
+		url = BASE_PATH + url;
+	}
+
+	let a = document.createElement('a');
+	a.href = url;
+	a.textContent = title;
+
+	if (a.host === location.host && a.pathname === location.pathname) {
+		a.classList.add('current');
+	}
+
+	if (a.host !== location.host) {
+		a.target = "_blank";
+	}
+
+	nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+	'afterbegin',
+	`
+	<label class="color-scheme">
+		Theme:
+		<select>
+		<option value="light dark">Automatic</option>
+		<option value="light">Light</option>
+		<option value="dark">Dark</option>
+		</select>
+	</label>
+	`
+);
+
+const select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+	const savedScheme = localStorage.colorScheme;
+	document.documentElement.style.setProperty('color-scheme', savedScheme);
+	select.value = savedScheme;
+}
+else {
+	document.documentElement.style.setProperty('color-scheme', 'light');
+	select.value = 'light';
+}
+
+select.addEventListener('input', function (event) {
+	const newScheme = event.target.value;
+	document.documentElement.style.setProperty('color-scheme', newScheme);
+	localStorage.colorScheme = newScheme;
 });
 
 export async function fetchJSON(url) {
-  try {
-    // Fetch the JSON file from the given URL
-    const response = await fetch(url);
-    console.log(response);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch projects: ${response.statusText}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching or parsing JSON data:', error);
-  }
+	try {
+		const response = await fetch(url);
+		
+		if (!response.ok) {
+			throw new Error(`Failed to fetch projects: ${response.statusText}`);
+		}
+		
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error fetching or parsing JSON data:', error);
+	}
 }
 
-export function renderProjects(project, containerElement) {
-  // Your code will go here
-  containerElement.innerHTML = '';
-  for (let project of projects) {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+	containerElement.innerHTML = '';
+	
+	for (let project of projects) {
 		const article = document.createElement('article');
 		
 		const heading = document.createElement(headingLevel);
