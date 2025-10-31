afc0391052cf98c6402dd76680a958ad28a1899c
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-31 01:57:18 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,18 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-const projectsTitle = document.querySelector('.projects-title');
+const projectsTitle = document.querySelector('h1');
 projectsTitle.textContent = `${projects.length} Projects`;
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
