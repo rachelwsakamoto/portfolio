cb2cdbabec3287987ba9f654a293d0de1ecdd95c
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-31 18:44:32 -0700

    idk

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,8 @@
 import { fetchJSON, renderProjects } from '../global.js';
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `${projects.length} Projects`;
