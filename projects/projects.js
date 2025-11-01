b9ee737dcf92ddd50985f4c173c8c7aac551086f
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-10-31 18:54:00 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -13,3 +13,27 @@ let arc = arcGenerator({
   startAngle: 0,
   endAngle: 2 * Math.PI,
 });
+
+
+let data = [1, 2];
+
+
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+
+
+let angle = 0;
+let arcData = [];
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+  d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+});
