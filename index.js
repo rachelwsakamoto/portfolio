9395e4637653dbf42ecfdf7b341503c5a305aa53
rachelwsakamoto <rwsakamoto@ucsd.edu>
2025-11-07 20:55:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (profileStats) {
 			<dt>Followers</dt><dd>${githubData.followers}</dd>
 			<dt>Following</dt><dd>${githubData.following}</dd>
 			<dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
-			<dt>Public Gists</dt><dd>${githubData.public_gists}</dd>
+			<dt>Public Gits</dt><dd>${githubData.public_gists}</dd>
 		</dl>
 	`;
 }
